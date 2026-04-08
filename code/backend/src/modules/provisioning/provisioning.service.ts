0089d50a3f6b628509e4c2b8a75cd32fb5b49a1f
Liaojiang Dev <liaojiang@local.dev>
2026-04-08 19:25:07 +0800

    Persist public order runtime state

--- code/backend/src/modules/provisioning/provisioning.service.ts
+++ code/backend/src/modules/provisioning/provisioning.service.ts
@@ -24,6 +24,7 @@ export class ProvisioningService {
     const subscription = this.vpnService.activateSubscription(
       input.accountId,
       input.planCode,
+      input.orderNo,
     );
     this.referralService.recordCompletedOrder({
       accountId: input.accountId,
